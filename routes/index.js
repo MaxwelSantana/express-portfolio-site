92265d64798d7fd10fedbb3e08339447f76a2498
Maxwel Santana <maxwelsant96@icloud.com>
2023-02-04 17:47:29 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET home page. */
+router.get('/products', function (req, res, next) {
+  res.render('products', { title: 'Products' });
+});
+
+/* GET home page. */
+router.get('/services', function (req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+/* GET home page. */
+router.get('/contact', function (req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
