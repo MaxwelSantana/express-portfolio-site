4d8aaa1ac70918b3ebf2baa57b49a733dc60f8df
Maxwel Santana <maxwelsant96@icloud.com>
2023-02-25 14:06:38 -0500

    setup session, flash and passport

--- server/config/app.js
+++ server/config/app.js
@@ -4,6 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
 // database setup
 let mongoose = require('mongoose');
 let DB = require('./db');
@@ -29,6 +36,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+//setup express session.
+app.use(
+  session({
+    secret: '3fbe31b5862629f8b1e79d8c63bc3931',
+    saveUninitialized: false,
+    resave: false,
+  }),
+);
+
+//initialize flash to maintain the error msg.
+app.use(flash());
+
+//initialize passport.
+app.use(passport.initialize());
+app.use(passport.session());
+// passport user configuration
+//create a user model instance
+let userModel = require('../models/user');
+let User = userModel.User;
+passport.use(User.createStrategy());
+//serialize and deserialize the user info
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use('/', indexRouter);
 
---
+++ server/models/user.js
@@ -0,0 +1,52 @@
+//require modules for the model
+let mongoose = require('mongoose');
+let passportLocalMongoose = require('passport-local-mongoose');
+let user = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      default: '',
+      trim: true,
+      required: 'username is required',
+    },
+    /*
+    password: {
+        type: String,
+        default: '',
+        trim: true,
+        required:'password is required'
+    }
+    */
+    email: {
+      type: String,
+      default: '',
+      trim: true,
+      required: 'email address is required',
+    },
+
+    displayName: {
+      type: String,
+      default: '',
+      trim: true,
+      required: 'Display Name is required',
+    },
+
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+
+    update: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+
+  {
+    collection: 'users',
+  },
+);
+//configure options for user model
+let options = { missingPasswordError: 'wrong/Missing Password' };
+user.plugin(passportLocalMongoose, options);
+module.exports.User = mongoose.model('user', user);
